f163744d8f1c1293de7528321e32c9e09e7c12f9
Akash Deolikar <you@example.com>
2025-07-16 18:28:39 +0530

    changes done

--- frontend/src/component/utility/HeroSlider.js
+++ frontend/src/component/utility/HeroSlider.js
@@ -3,17 +3,17 @@ import './HeroSlider.css';
 import SlideItem from './SlideItem';
 
 // import img1 from '../utility/slideasset/blueberry.png';
-import img1 from '../utility/ertiga.png';
+import img1 from '../utility/im3.png';
 import img2 from '../utility/slideasset/coke.png';
-import img3 from '../utility/slideasset/berry.png';
+import img3 from '../utility/im41.png';
 
-import bg1 from './slideasset/s-bg1.jpg';
+import bg1 from './slideasset/s-bg3.jpg';
 import bg2 from './slideasset/s-bg2.jpg';
-import bg3 from './slideasset/s-bg3.jpg';
+import bg3 from './slideasset/s-bg1.jpg';
 
-import downbg1 from './slideasset/rocks1.png';
+import downbg1 from './slideasset/rocks3.png';
 import downbg2 from './slideasset/rock2.png';
-import downbg3 from './slideasset/rocks3.png';
+import downbg3 from './slideasset/rocks1.png';
 
 const slides = [
   {
@@ -26,9 +26,10 @@ const slides = [
   {
     title: "Tomorrow's Mobility",
     subtitle: "Driving the future, one innovation at a time.",
-    img: img2,
+    img: null,
     bgImage: bg2,
     downimg: downbg2,
+    video: "https://www.tatamotors.com/wp-content/themes/TataMotors/video/TML-Desktop-video.mp4", 
   },
   {
     title: "Explore the Road",
@@ -59,6 +60,7 @@ const HeroSlider = () => {
         img={currentSlide.img}
         downimg={currentSlide.downimg}
         bgImage={currentSlide.bgImage}
+        video={currentSlide.video}
       />
 
       <div className="slider-nav">
--- frontend/src/component/utility/SlideItem.js
+++ frontend/src/component/utility/SlideItem.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './SlideItem.css';
 
-const SlideItem = ({ title, subtitle, img, bgImage, downimg }) => {
+const SlideItem = ({ title, subtitle, img, bgImage, downimg, video }) => {
+  const isMp4 = video?.endsWith(".mp4");
+
   return (
-    <div className="slide-item" style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <div
+      className="slide-item"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
       <div className="slide-background-title">{title}</div>
 
       <div className="slide-content-wrapper">
@@ -12,21 +21,53 @@ const SlideItem = ({ title, subtitle, img, bgImage, downimg }) => {
           <p>{subtitle}</p>
         </div>
 
-        <div className="slide-img">
-          <img src={img} alt={title} />
-          <div className="img-glow-circle"></div>
-        </div>
+        {/* Show image if no video */}
+        {!video && img && (
+          <div className="slide-img">
+            <img src={img} alt={title} />
+            <div className="img-glow-circle"></div>
+          </div>
+        )}
       </div>
 
-      {/* ✅ Move here */}
+      {/* ✅ Show MP4 video if available */}
+      {video && isMp4 && (
+        <div className="slide-video-wrapper">
+          <video
+            src={video}
+            controls
+            autoPlay
+            muted
+            loop
+            style={{ width: "100%", borderRadius: "12px" }}
+          />
+        </div>
+      )}
+
+      {/* ✅ Show YouTube iframe if it's not an MP4 */}
+      {video && !isMp4 && (
+        <div className="slide-video-wrapper">
+          <iframe
+            width="100%"
+            height="400"
+            src={video}
+            title="Embedded Video"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </div>
+      )}
+
       {downimg && (
         <div className="slide-down-img">
           <img src={downimg} alt="Decorative bottom" />
         </div>
       )}
     </div>
-
   );
 };
+
+
 
 export default SlideItem;
